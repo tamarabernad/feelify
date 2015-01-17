727487c0f59b8671206875f57a666fd2e954a5e0
Victor <victor@fishbrain.com>
2015-01-17 10:34:03 +0100

    Refactor receiver

--- app/src/main/java/es/moodbox/feelify/fragments/MoodCreationFragment.java
+++ app/src/main/java/es/moodbox/feelify/fragments/MoodCreationFragment.java
@@ -1,6 +1,5 @@
 package es.moodbox.feelify.fragments;
 
-import android.app.Activity;
 import android.app.DownloadManager;
 import android.app.Fragment;
 import android.content.BroadcastReceiver;
@@ -18,84 +17,29 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.io.File;
-
 import es.moodbox.feelify.R;
 import es.moodbox.feelify.utils.AppConstants;
 import es.moodbox.feelify.utils.DLManager;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MoodCreationFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MoodCreationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MoodCreationFragment extends Fragment {
     private WebView mWebView;
     private EditText mTextEdit;
     private String mUrl;
-    private View mView;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        BroadcastReceiver receiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String action = intent.getAction();
-                if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
-
-
-                    long downloadId = intent.getLongExtra(
-                            DownloadManager.EXTRA_DOWNLOAD_ID, 0);
-                    DownloadManager.Query query = new DownloadManager.Query();
-                    query.setFilterById(downloadId);
-                    Cursor c = DLManager.manger.query(query);
-                    if (c.moveToFirst()) {
-                        int columnIndex = c
-                                .getColumnIndex(DownloadManager.COLUMN_STATUS);
-                        if (DownloadManager.STATUS_SUCCESSFUL == c
-                                .getInt(columnIndex)) {
-
-                            String uriString = c
-                                    .getString(c
-                                            .getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
-                            Log.d("MoodCreationFragment",uriString);
-
-                            //File mFile = new File(uriString);
-                            sendFileWithUri(uriString);
-
-                        }
-                    }
-                }
-            }
-        };
-
-        getActivity().registerReceiver(receiver, new IntentFilter(
-                DownloadManager.ACTION_DOWNLOAD_COMPLETE));
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         View v =inflater.inflate(R.layout.fragment_mood_creation, container, false);
-        mView = v;
         mWebView =(WebView) v.findViewById(R.id.webView);
+
         Button btShare =(Button) v.findViewById(R.id.btShare);
         mTextEdit = (EditText)v.findViewById(R.id.editText);
+
         btShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DLManager.useDownloadManager(mUrl, AppConstants.IMG_DIR, AppConstants.IMG_NAME+".gif", getActivity());
-                /*try {
-                    downloadImageWithUrl(mUrl);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }*/
 
             }
         });
@@ -111,10 +55,52 @@ public class MoodCreationFragment extends Fragment {
         sendIntent.putExtra(Intent.EXTRA_STREAM,Uri.parse(imageUrl));
         startActivity(sendIntent);
     }
+
     @Override
     public void onResume() {
         super.onResume();
+
+        getActivity().registerReceiver(receiver, new IntentFilter(
+                DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
         mUrl = getActivity().getIntent().getStringExtra("url");
         mWebView.loadUrl(mUrl);
     }
+
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(receiver);
+    }
+
+    public BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+
+
+                long downloadId = intent.getLongExtra(
+                        DownloadManager.EXTRA_DOWNLOAD_ID, 0);
+                DownloadManager.Query query = new DownloadManager.Query();
+                query.setFilterById(downloadId);
+                Cursor c = DLManager.manger.query(query);
+                if (c.moveToFirst()) {
+                    int columnIndex = c
+                            .getColumnIndex(DownloadManager.COLUMN_STATUS);
+                    if (DownloadManager.STATUS_SUCCESSFUL == c
+                            .getInt(columnIndex)) {
+
+                        String uriString = c
+                                .getString(c
+                                        .getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+                        Log.d("MoodCreationFragment",uriString);
+
+                        //File mFile = new File(uriString);
+                        sendFileWithUri(uriString);
+
+                    }
+                }
+            }
+        }
+    };
 }
