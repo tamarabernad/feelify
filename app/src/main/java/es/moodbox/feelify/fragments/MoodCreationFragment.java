b85f0cf8efe7f11e0dc9315836e79c5a43c6b7ba
Victor <victor@fishbrain.com>
2015-01-17 12:38:31 +0100

    Remove download and READ permission

--- app/src/main/java/es/moodbox/feelify/fragments/MoodCreationFragment.java
+++ app/src/main/java/es/moodbox/feelify/fragments/MoodCreationFragment.java
@@ -1,13 +1,9 @@
 package es.moodbox.feelify.fragments;
 
-import android.app.DownloadManager;
 import android.app.Fragment;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,11 +13,17 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.File;
+import java.net.URL;
+
 import es.moodbox.feelify.R;
+import es.moodbox.feelify.services.FileDownloader;
 import es.moodbox.feelify.utils.AppConstants;
-import es.moodbox.feelify.utils.DLManager;
 
 public class MoodCreationFragment extends Fragment {
+
+    private final static String TAG = MoodCreationFragment.class.getSimpleName();
+
     private WebView mWebView;
     private EditText mTextEdit;
     private String mUrl;
@@ -39,20 +41,59 @@ public class MoodCreationFragment extends Fragment {
         btShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DLManager.useDownloadManager(mUrl, AppConstants.IMG_DIR, AppConstants.IMG_NAME+".gif", getActivity());
-
+                try {
+                    new DownloadFilesTask().execute(new URL(mUrl));
+                } catch (Exception e) {
+                    Log.e(TAG, "Ups...."+e.getMessage());
+                }
             }
         });
         return v;
     }
-    private void sendFileWithUri(String imageUrl){
+
+    public class DownloadFilesTask extends AsyncTask<URL, Integer, File> {
+
+        protected File doInBackground(URL... urls) {
+            int count = urls.length;
+            long totalSize = 0;
+            File tmp = null;
+            for (int i = 0; i < count; i++) {
+                tmp = FileDownloader.download(getActivity(), AppConstants.IMG_NAME, ".gif", urls[i]);
+                if (tmp != null) {
+                    totalSize += tmp.length();
+                    Log.d(TAG, "Downloaded total: "+totalSize+ " url: "+urls[i].toString());
+                }
+
+                publishProgress((int) ((i / (float) count) * 100));
+                // Escape early if cancel() is called
+                if (isCancelled()) break;
+            }
+            return tmp;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+        }
+
+        protected void onPostExecute(File result) {
+            Uri path = Uri.fromFile(result);
+	        Log.d(TAG, "Download done, sharing file: "+path);
+	        try {
+		        sendFileWithUri(result);
+	        } catch (Exception e) {
+		        Log.e(TAG, "ups: "+e.getMessage());
+		        e.printStackTrace();
+	        }
+        }
+    }
+
+    private void sendFileWithUri(File image) throws Exception{
         String message = mTextEdit.getText() + "by feely";
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
         sendIntent.putExtra(Intent.EXTRA_TEXT, message);
         sendIntent.setType("image/*");
         //Uri path = Uri.fromFile(new File("android.resource://com.android.mypackage/drawable/arrow.png"));
-        sendIntent.putExtra(Intent.EXTRA_STREAM,Uri.parse(imageUrl));
+        sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(image));
         startActivity(sendIntent);
     }
 
@@ -60,47 +101,7 @@ public class MoodCreationFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
-        getActivity().registerReceiver(receiver, new IntentFilter(
-                DownloadManager.ACTION_DOWNLOAD_COMPLETE));
-
         mUrl = getActivity().getIntent().getStringExtra("url");
         mWebView.loadUrl(mUrl);
     }
-
-    public void onPause() {
-        super.onPause();
-        getActivity().unregisterReceiver(receiver);
-    }
-
-    public BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
-
-
-                long downloadId = intent.getLongExtra(
-                        DownloadManager.EXTRA_DOWNLOAD_ID, 0);
-                DownloadManager.Query query = new DownloadManager.Query();
-                query.setFilterById(downloadId);
-                Cursor c = DLManager.manger.query(query);
-                if (c.moveToFirst()) {
-                    int columnIndex = c
-                            .getColumnIndex(DownloadManager.COLUMN_STATUS);
-                    if (DownloadManager.STATUS_SUCCESSFUL == c
-                            .getInt(columnIndex)) {
-
-                        String uriString = c
-                                .getString(c
-                                        .getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
-                        Log.d("MoodCreationFragment",uriString);
-
-                        //File mFile = new File(uriString);
-                        sendFileWithUri(uriString);
-
-                    }
-                }
-            }
-        }
-    };
 }
